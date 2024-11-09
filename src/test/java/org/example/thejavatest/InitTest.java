88848513d4ae4d153d0885946e691d9b5db7cf9e
Jealyang <jaelyangchoi@gmail.com>
2024-11-10 00:23:30 +0900

    테스트 이름 표시하기

--- src/test/java/org/example/thejavatest/InitTest.java
+++ src/test/java/org/example/thejavatest/InitTest.java
@@ -4,19 +4,22 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//전체 적용 : 메소드 명의 언더스코어를 공백으로 치환
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class InitTest {
 
     @Test
-    void create() {
+    @DisplayName("스터디 객체 만들기")
+    void create_new_study() {
         Init study = new Init();
         assertNotNull(study);
-        System.out.println("StudyTest.create");
+        System.out.println("InitTest.create_new_study");
     }
 
     @Test
     @Disabled //깨지는 테스트에 활용
-    void create1() {
-        System.out.println("StudyTest.create1");
+    void create_new_study_again() {
+        System.out.println("InitTest.create_new_study_again");
     }
 
     //전체 테스트를 실행하기 전에 한번 실행
