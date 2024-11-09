b8cccd68c5a9ba7469acb1bb18799b3aed57e29a
Jealyang <jaelyangchoi@gmail.com>
2024-11-09 23:38:37 +0900

    initial commit

---
+++ src/test/java/org/example/thejavatest/TheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.thejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
