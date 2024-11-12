ba918be114e1418fc1a06f5504d8893835db1d3c
Jealyang <jaelyangchoi@gmail.com>
2024-11-12 17:40:23 +0900

    stubbing

--- src/test/java/org/example/thejavatest/study/StudyServiceTest.java
+++ src/test/java/org/example/thejavatest/study/StudyServiceTest.java
@@ -44,6 +44,19 @@ class StudyServiceTest {
     }
 
     @Test
+    void stubbingPractice(){
+        Study study = new Study(10, "테스트");
+
+        // TODO memberService 객체에 findById 메소드를 1L 값으로 호출하면 Optional.of(member) 객체를 리턴하도록 Stubbing
+        // TODO studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+
+        studyService.createNewStudy(1L, study);
+
+        assertNotNull(study.getOwner());
+        assertEquals(member, study.getOwner());
+
+    }
+    @Test
     void createNewStudy(@Mock MemberService memberService
             , @Mock StudyRepository studyRepository) {
         StudyService studyService = new StudyService(memberService, studyRepository);
