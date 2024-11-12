a085279ae06d5ff6e9797ec8fd09c6bfb5c979a8
Jealyang <jaelyangchoi@gmail.com>
2024-11-12 17:37:44 +0900

    stubbing

--- src/test/java/org/example/thejavatest/study/StudyServiceTest.java
+++ src/test/java/org/example/thejavatest/study/StudyServiceTest.java
@@ -1,5 +1,8 @@
 package org.example.thejavatest.study;
 
+import org.assertj.core.api.Assertions;
+import org.example.thejavatest.domain.Member;
+import org.example.thejavatest.domain.Study;
 import org.example.thejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,20 +10,56 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
-    @Mock
-    StudyRepository studyRepository;
+    @Test
+    void stubbing(@Mock MemberService memberService) {
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@Test.com");
+
+        //stubbing
+        Mockito.when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        Mockito.doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThat(member).isEqualTo(memberService.findById(1L).get());
+        assertThrows(IllegalArgumentException.class, () -> memberService.validate(1L));
+
+        //stubbing : 여러번 호출될 때 각기 다르게 행동하도록 지정
+        Mockito.when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new IllegalArgumentException())
+                .thenReturn(Optional.empty());
+
+        assertEquals(member, memberService.findById(7L).get());
+        assertThrows(IllegalArgumentException.class, () -> memberService.findById(1L));
+        assertEquals(Optional.empty(), memberService.findById(1L));
+    }
 
     @Test
-    void createStudyService(){
-        MemberService memberService = Mockito.mock(MemberService.class);
-        //인터페이스만 있고 구현체가 없을 때 mocking
+    void createNewStudy(@Mock MemberService memberService
+            , @Mock StudyRepository studyRepository) {
         StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("test@Test.com");
+
+        //stubbing
+        Mockito.when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        Mockito.when(memberService.findById(2L)).thenThrow(new RuntimeException());
+        Mockito.doThrow(new IllegalArgumentException()).when(memberService).validate(any());
+
+        assertThat(member).isEqualTo(memberService.findById(1L).get());
+        assertThrows(RuntimeException.class, () -> memberService.findById(2L).get());
+        assertThrows(IllegalArgumentException.class, () -> memberService.validate(1L));
     }
 
 
